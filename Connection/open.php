b238fdedaee48679c60e96af13f5d7af84e3fc99
HieuBlue <68227017+hieuk33hl@users.noreply.github.com>
2022-02-09 22:14:58 +0700

    new

--- Connection/open.php
+++ Connection/open.php
@@ -1,3 +1,3 @@
 <?php 
-    $conn = mysqli_connect('localhost','root','','da1');
+    $conn = mysqli_connect('bvptdk7av7hrmseqfizz-mysql.services.clever-cloud.com','uyww3ji1nhiyxxol','GjqEvjM8PemYluqMloGW','bvptdk7av7hrmseqfizz');
 ?>
